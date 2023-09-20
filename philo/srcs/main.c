3635186f7b1d230fdd52c6418551108192cd8093
habvi <93956895+habvi@users.noreply.github.com>
2023-09-20 19:45:50 +0900

    add(thread): try create single thread. just print args.

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -1,15 +1,46 @@
 #include <pthread.h>
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "philo.h"
 #include "utils.h"
 
+static void	*thread_func(void *args)
+{
+	printf("\n--- thread start ---\n");
+	put_args(args);
+	printf("--- thread end ---\n\n");
+	return (NULL);
+}
 
+// todo: exit -> return error
+static pthread_t	create_thread(t_args *args)
+{
+	pthread_t	thread;
+
+	if (pthread_create(&thread, NULL, thread_func, args) != THREAD_SUCCESS)
+	{
+		perror("pthread_create");
+		exit(EXIT_FAILURE);
+	}
+	return (thread);
+}
+
+// todo: exit -> return error
+static void	wait_thread(pthread_t thread)
+{
+	if (pthread_join(thread, NULL) != THREAD_SUCCESS)
+	{
+		perror("pthread_join");
+		exit(EXIT_FAILURE);
+	}
+}
 
 static void	run_philosophers(t_args args)
 {
-	(void)args;
+	pthread_t	thread;
+
+	thread = create_thread(&args);
+	wait_thread(thread);
 }
 
 int	main(int argc, char *argv[])
