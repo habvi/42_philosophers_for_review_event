0f2cdf46eebce2b03a2262d7dd9bd5299e093a1a
habvi <93956895+habvi@users.noreply.github.com>
2023-09-25 18:18:40 +0900

    add(threads): pass thread_info to each threads. only eating once.

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -1,16 +1,61 @@
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "philo.h"
-#include "result.h"
 #include "utils.h"
 
-static void	*thread_func(void *args)
+// todo: return
+static void	*thread_func(void *thread_args)
 {
-	printf("\n--- thread start ---\n");
-	put_args(args);
-	printf("--- thread end ---\n\n");
+	t_thread_info	*thread_info;
+	t_args			*args;
+
+	thread_info = (t_thread_info *)thread_args;
+	args = thread_info->args;
+
+	if (pthread_mutex_lock(&args->left_fork) != MUTEX_SUCCESS)
+	{
+		perror("pthread_mutex_lock");
+		return (NULL);
+	}
+	printf("%d has taken a left fork\n", thread_info->philo_id);
+
+	if (pthread_mutex_lock(&args->right_fork) != MUTEX_SUCCESS)
+	{
+		perror("pthread_mutex_lock");
+		return (NULL);
+	}
+	printf("%d has taken a right fork\n", thread_info->philo_id);
+
+	printf("%d is eating\n", thread_info->philo_id);
+	usleep(args->time_to_eat * 1000);
+
+	if (pthread_mutex_unlock(&args->left_fork) != MUTEX_SUCCESS)
+	{
+		perror("pthread_mutex_unlock");
+		return (NULL);
+	}
+	if (pthread_mutex_unlock(&args->right_fork) != MUTEX_SUCCESS)
+	{
+		perror("pthread_mutex_unlock");
+		return (NULL);
+	}
+	printf("%d is thinking\n", thread_info->philo_id);
+	free(thread_info);
 	return (NULL);
+}
+
+static t_thread_info	*set_thread_info(int i, t_args *args)
+{
+	t_thread_info	*thread_info;
+
+	thread_info = (t_thread_info *)malloc(sizeof(t_thread_info) * 1);
+	// todo: error
+	if (thread_info == NULL)
+		return (NULL);
+	thread_info->philo_id = i;
+	thread_info->args = args;
+	return (thread_info);
 }
 
 // todo: exit -> return error
@@ -18,6 +63,7 @@ static pthread_t	*create_threads(t_args *args)
 {
 	pthread_t	*threads;
 	int			i;
+	t_thread_info	*thread_info;
 
 	threads = (pthread_t *)malloc(sizeof(pthread_t) * args->number_of_philosophers);
 	if (threads == NULL)
@@ -25,7 +71,8 @@ static pthread_t	*create_threads(t_args *args)
 	i = 0;
 	while (i < args->number_of_philosophers)
 	{
-		if (pthread_create(&threads[i], NULL, thread_func, args) != THREAD_SUCCESS)
+		thread_info = set_thread_info(i, args);
+		if (pthread_create(&threads[i], NULL, thread_func, thread_info) != THREAD_SUCCESS)
 		{
 			perror("pthread_create");
 			exit(EXIT_FAILURE);
