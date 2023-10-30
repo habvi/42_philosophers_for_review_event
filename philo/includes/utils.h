557646e595c553722de50f811752ea9198f19fac
habvi <93956895+habvi@users.noreply.github.com>
2023-10-31 03:38:59 +0900

    fmt

--- philo/includes/utils.h
+++ philo/includes/utils.h
@@ -6,23 +6,23 @@
 
 # define TIME_ERROR (-1)
 
-typedef struct s_args	t_args;
-typedef struct s_philo	t_philo;
+typedef struct s_args		t_args;
+typedef struct s_philo		t_philo;
 typedef struct s_philo_var	t_philo_var;
 
 /* utils */
-bool	    ft_atoi(const char *str, int *num);
-void	    *ft_free(void **ptr);
-bool	    ft_isdigit(int c);
+bool		ft_atoi(const char *str, int *num);
+void		*ft_free(void **ptr);
+bool		ft_isdigit(int c);
 
 /* debug */
-void	    put_args(const t_args *args);
-void	    put(const t_philo *philo, const char *message);
+void		put_args(const t_args *args);
+void		put(const t_philo *philo, const char *message);
 
 /* time */
-long	    get_current_time(void);
-long	    get_elapsed_time(const t_philo *philo);
-long	    get_elapsed_cycle_time(const t_philo *philo);
+long		get_current_time(void);
+long		get_elapsed_time(const t_philo *philo);
+long		get_elapsed_cycle_time(const t_philo *philo);
 t_result	set_start_time_of_cycle(t_philo_var *philo_var);
 
 #endif
