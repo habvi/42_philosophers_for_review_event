3e0578284c4beda920abc73765eab05b642e5959
habvi <93956895+habvi@users.noreply.github.com>
2023-10-31 04:17:03 +0900

    separate file(forks)

---
+++ philo/srcs/forks.c
@@ -0,0 +1,54 @@
+#include "philo.h"
+
+// static void	interval(const int id)
+// {
+// 	if (id % 2)
+// 		usleep(50);
+// }
+
+static t_result	take_fork(pthread_mutex_t *fork)
+{
+	if (pthread_mutex_lock(fork) != MUTEX_SUCCESS)
+		return (FAILURE);
+	return (SUCCESS);
+}
+
+static t_result	put_fork(pthread_mutex_t *fork)
+{
+	if (pthread_mutex_unlock(fork) != MUTEX_SUCCESS)
+		return (FAILURE);
+	return (SUCCESS);
+}
+
+t_result	put_two_forks(const t_philo *philo)
+{
+	if (put_fork(philo->right_fork) == FAILURE)
+		return (FAILURE);
+	if (put_fork(philo->left_fork) == FAILURE)
+		return (FAILURE);
+	return (SUCCESS);
+}
+
+t_result	take_two_forks(const t_philo *philo)
+{
+	// interval(philo->id);
+	if (take_fork(philo->left_fork) == FAILURE)
+		return (FAILURE);
+	if (is_any_philo_died(philo, __LINE__, __func__))
+	{
+		if (put_fork(philo->left_fork) == FAILURE)
+			return (FAILURE);
+		return (BREAK);
+	}
+	put_log(philo, MSG_FORK);
+	if (take_fork(philo->right_fork) == FAILURE)
+		return (FAILURE);
+	if (is_any_philo_died(philo, __LINE__, __func__))
+	{
+		if (put_two_forks(philo) == FAILURE)
+			return (FAILURE);
+		return (BREAK);
+	}
+	put_log(philo, MSG_FORK);
+	return (SUCCESS);
+}
