f7be5165267c152a58fe2e4b077c5d866959ba0f
habvi <93956895+habvi@users.noreply.github.com>
2023-11-17 06:28:57 +0900

    update(adjust_simulation_start): wait using time_to_think

--- philo/srcs/philo_cycle.c
+++ philo/srcs/philo_cycle.c
@@ -28,8 +28,26 @@ void	*philo_solo_cycle(void *thread_args)
 
 static void	adjust_simulation_start(const t_philo *philo)
 {
-	if (philo->id % 2 == 1)
-		usleep(200);
+	const unsigned int	num_of_philos = philo->args.num_of_philos;
+	const unsigned int	id = philo->id;
+	unsigned int		x;
+	const int64_t		cycle_time = philo->args.time_to_eat \
+									+ philo->args.time_to_sleep \
+									+ philo->args.time_to_think;
+
+	if (num_of_philos % 2 == 0)
+	{
+		if (philo->id % 2 == 1)
+			usleep(200);
+	}
+	else
+	{
+		if (id % 2 == 0)
+			x = id / 2;
+		else
+			x = (num_of_philos / 2 + 1) + id / 2;
+		usleep((philo->args.time_to_think * x) % cycle_time);
+	}
 }
 
 void	*philo_cycle(void *thread_args)
